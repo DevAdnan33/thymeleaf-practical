32e684b6565cc006070b5850804cdbf49d4e4e0d
Adnan Rashid <adnanrashid1111@gmail.com>
2024-07-14 15:59:57 +0500

    using iteration in thymeleaf

--- src/main/java/com/thymeleafpractical/thymeleaf_practical/controller/MyController.java
+++ src/main/java/com/thymeleafpractical/thymeleaf_practical/controller/MyController.java
@@ -1,9 +1,11 @@
 package com.thymeleafpractical.thymeleaf_practical.controller;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,5 +16,12 @@ public class MyController {
         model.addAttribute("name", "Adnan");
         model.addAttribute("date", new Date()).toString();
         return "about";
+    }
+
+    @GetMapping("/example-loop.html")
+    public String itrateHandler(Model model) {
+        List<String> names = List.of("Adnan", "Irfan", "Nauman", "Khalid");
+        model.addAttribute("names", names);
+        return "example-loop";
     }
 }
